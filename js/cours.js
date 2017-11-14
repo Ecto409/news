b42e5bb6fefb74caa3721d2c0164c37b0e4a8835
Votre nom ou pseudoEcto409 <sgoffin1@hotmail.com>
2017-11-14 19:07:14 +0100

    Project completed

--- js/cours.js
+++ js/cours.js
@@ -7,7 +7,7 @@ ajaxGet("https://newsapi.org/v1/articles?source=the-next-web&sortBy=latest&apiKe
   var author0 = articles.articles[0].author;
   var source0 = articles.source;
   var source0Elt = document.getElementById('source0');
-  source0Elt.textContent = "ON" + source0 + ", by " + author0;
+  source0Elt.textContent = "ON " + source0 + ", by " + author0;
   var title0 = articles.articles[0].title;
   var title0Elt = document.getElementById('title0');
   title0Elt.textContent = title0;
@@ -27,7 +27,7 @@ ajaxGet("https://newsapi.org/v1/articles?source=the-verge&sortBy=top&apiKey=b907
   var bauthor0 = articles.articles[0].author;
   var bsource0 = articles.source;
   var bsource0Elt = document.getElementById('bsource0');
-  bsource0Elt.textContent = "ON" + bsource0 + ", by " + bauthor0;
+  bsource0Elt.textContent = "ON " + bsource0 + ", by " + bauthor0;
   var btitle0 = articles.articles[0].title;
   var btitle0Elt = document.getElementById('btitle0');
   btitle0Elt.textContent = btitle0;
@@ -48,7 +48,7 @@ ajaxGet("https://newsapi.org/v1/articles?source=techcrunch&sortBy=latest&apiKey=
   var cauthor0 = articles.articles[0].author;
   var csource0 = articles.source;
   var csource0Elt = document.getElementById('csource0');
-  csource0Elt.textContent = "ON" + csource0 + ", by " + cauthor0;
+  csource0Elt.textContent = "ON " + csource0 + ", by " + cauthor0;
   var ctitle0 = articles.articles[0].title;
   var ctitle0Elt = document.getElementById('ctitle0');
   ctitle0Elt.textContent = ctitle0;
@@ -69,7 +69,7 @@ ajaxGet("https://newsapi.org/v1/articles?source=recode&sortBy=top&apiKey=b907a5d
   var dauthor0 = articles.articles[0].author;
   var dsource0 = articles.source;
   var dsource0Elt = document.getElementById('dsource0');
-  dsource0Elt.textContent = "ON" + dsource0 + ", by " + dauthor0;
+  dsource0Elt.textContent = "ON " + dsource0 + ", by " + dauthor0;
   var dtitle0 = articles.articles[0].title;
   var dtitle0Elt = document.getElementById('dtitle0');
   dtitle0Elt.textContent = dtitle0;
@@ -90,7 +90,7 @@ ajaxGet("https://newsapi.org/v1/articles?source=hacker-news&sortBy=top&apiKey=b9
   var eauthor0 = articles.articles[0].author;
   var esource0 = articles.source;
   var esource0Elt = document.getElementById('esource0');
-  esource0Elt.textContent = "ON" + esource0 + ", by " + eauthor0;
+  esource0Elt.textContent = "ON " + esource0 + ", by " + eauthor0;
   var etitle0 = articles.articles[0].title;
   var etitle0Elt = document.getElementById('etitle0');
   etitle0Elt.textContent = etitle0;
